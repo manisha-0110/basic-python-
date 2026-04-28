0c8578cd1d7c13f20ecaf542e44c1547fd8b8c8f
manisha-0110 <kintalimanisha22@gmail.com>
2026-04-28 21:18:52 +0530

    Enhance days lived calculation with seconds
    Added calculation for total seconds lived.

--- days_Lived.py
+++ days_Lived.py
@@ -2,3 +2,13 @@ from datetime import date
 a=date(2029,4,12)
 b=date(6089,3,3)
 print("no of days he lived: ",(b-a))
+
+
+#--------with seconds----------
+
+
+from datetime import date
+a=date(2029,4,12)
+b=date(6089,3,3)
+print("no of days he lived: ",(b - a).days)
+print("no of seconds he lived: ", (b - a).total_seconds())
