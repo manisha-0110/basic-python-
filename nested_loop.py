4b2854eab9186e4238578aef6eb14b66156b2d81
manisha-0110 <kintalimanisha22@gmail.com>
2026-04-28 21:20:40 +0530

    Add nested loops to print and modify variable

---
+++ nested_loop.py
@@ -0,0 +1,14 @@
+#-----1-----
+for i in range (5):
+    for j in range(4):
+	print("hi" ,end=" ")
+print()
+
+#------2------
+
+a=10
+for i in range(4):
+	for j in range(i+1):
+	   a=a+1
+print("ACE")
+print(a)
